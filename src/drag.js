5383a0b4209fac39fff12f673c69fe86a3a7bcee
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-07-20 20:14:38 +0530

    refactor: update drag directive code 🎉

---
+++ src/drag.js
@@ -0,0 +1,142 @@
+const createOverlay = (e, el, _data) => {
+  const overlay = document.createElement('div');
+  overlay.setAttribute(
+    'style',
+    `
+    width: 100vw; 
+    height: 100vh; 
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 10000;
+  `,
+  );
+  overlay.addEventListener('mouseup', (e) => mouseup(e, el, _data));
+  overlay.addEventListener('mousedown', (e) => mousedown(e, el, _data));
+  overlay.addEventListener('mousemove', (e) => mousemove(e, el, _data));
+  document.body.appendChild(overlay);
+
+  return overlay;
+};
+
+const checkIfIdInPath = (id, path) => {
+  for (let i = 0; i < path.length; i++) {
+    if (path[i].id === id) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const adjustElementZIndex = (el, index) => {
+  el.style.zIndex = index;
+};
+
+const mousedown = (e, el, _data) => {
+  // if the user set a argument to v-drag,
+  // it means they only want a specific area to be draggable
+  // eg: `v-drag:drag-header` means only the element with
+  // id="drag-header" should be draggable.
+  // If the user clicked another area, do nothing.
+  if (
+    _data.draggableElementId &&
+    !checkIfIdInPath(_data.draggableElementId, e.path)
+  ) {
+    return;
+  }
+
+  if (_data.overlay) {
+    _data.overlay.remove();
+  }
+  // set the width each click
+  // just in case it changed since last time (by external plugin, for example)
+  _data.width = el.offsetWidth;
+  _data.height = el.offsetHeight;
+  _data.down = true;
+  _data.initialX = e.clientX;
+  _data.initialY = e.clientY;
+  const overlay = createOverlay(e, el, _data);
+  _data.overlay = overlay;
+  adjustElementZIndex(el, 10001);
+};
+
+const mouseup = (e, el, _data) => {
+  _data.down = false;
+  if (!_data.overlay) {
+    return;
+  }
+
+  _data.overlay.removeEventListener('mouseup', mouseup);
+  _data.overlay.removeEventListener('mousedown', mousedown);
+  _data.overlay.removeEventListener('mousemove', mousemove);
+  _data.overlay.remove();
+  adjustElementZIndex(el, _data.initialZIndex);
+
+  setDraggerOffset(el, _data);
+};
+
+const reachedLeft = (el, _data, movingLeft) => {
+  return el.offsetLeft + _data.width >= window.innerWidth && !movingLeft;
+};
+
+const reachedRight = (el, _data, movingRight) => {
+  return el.offsetLeft <= 0 && !movingRight;
+};
+
+const reachedTop = (el, _data, movingUp) => {
+  return el.offsetTop <= 0 && !movingUp;
+};
+
+const reachedBottom = (el, _data, movingDown) => {
+  return el.offsetTop + _data.height >= window.innerHeight && !movingDown;
+};
+
+const mousemove = (e, el, _data) => {
+  if (_data.down) {
+    const movingLeft = _data.cursorPreviousX > e.clientX;
+    const movingRight = _data.cursorPreviousX < e.clientX;
+    const movingUp = _data.cursorPreviousY < e.clientY;
+    const movingDown = _data.cursorPreviousY > e.clientY;
+
+    if (
+      _data.constraintToWindow &&
+      (reachedLeft(el, _data, movingLeft) ||
+        reachedRight(el, _data, movingRight))
+    ) {
+      // do now allow moving outside the window horizontally
+    } else {
+      el.style.left =
+        _data.draggerOffsetLeft + (e.clientX - _data.initialX) + 'px';
+    }
+    if (
+      _data.constraintToWindow &&
+      (reachedTop(el, _data, movingUp) || reachedBottom(el, _data, movingDown))
+    ) {
+      // do now allow moving outside the window vertically
+    } else {
+      el.style.top =
+        _data.draggerOffsetTop + (e.clientY - _data.initialY) + 'px';
+    }
+  }
+  _data.cursorPreviousX = e.clientX;
+  _data.cursorPreviousY = e.clientY;
+};
+
+const setDraggerOffset = (el, _data) => {
+  _data.draggerOffsetLeft = el.offsetLeft;
+  _data.draggerOffsetTop = el.offsetTop;
+};
+
+export {
+  createOverlay,
+  checkIfIdInPath,
+  adjustElementZIndex,
+  mousedown,
+  mouseup,
+  reachedLeft,
+  reachedRight,
+  reachedTop,
+  reachedBottom,
+  mousemove,
+  setDraggerOffset,
+};
